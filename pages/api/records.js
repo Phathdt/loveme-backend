ca27aa9aeadec1086953c098f1dd2aa65318515d
Phathdt <phathdt379@gmail.com>
2023-02-13 23:40:35 +0700

    process google spreadsheet

--- pages/api/records.js
+++ pages/api/records.js
@@ -1,4 +1,21 @@
 import NextCors from 'nextjs-cors'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID)
+let init = false
+let sheet = null
+
+async function initGoogleSheetClient() {
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  })
+
+  await doc.loadInfo()
+
+  sheet = doc.sheetsByIndex[0]
+  init = true
+}
 
 export default async function handler(req, res) {
   await NextCors(req, res, {
@@ -8,9 +25,14 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 
+  if (!init) {
+    await initGoogleSheetClient()
+  }
+
   switch (req.method) {
     case 'POST':
-      console.log(req.body)
+      // append rows
+      await sheet.addRow(req.body)
 
       res.status(200).json({ msg: 'ok' })
       break
