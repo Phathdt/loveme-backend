36cb33dfce0b730ac8e635760f6669dd42c58b95
Phathdt <phathdt379@gmail.com>
2023-02-13 20:29:26 +0700

    add api records

--- pages/api/records.js
+++ pages/api/records.js
@@ -8,7 +8,19 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 
-  console.log(req.body)
+  switch (req.method) {
+    case 'POST':
+      console.log(req.body)
 
-  res.status(200).json({ name: 'ok' })
+      res.status(200).json({ msg: 'ok' })
+      break
+
+    case 'GET':
+      res.status(200).json({ records: [] })
+      break
+
+    default:
+      res.status(200).json({ msg: 'ok' })
+      break
+  }
 }
